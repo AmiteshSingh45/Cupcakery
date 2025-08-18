0b3af10c8f92d18d6438e698362b485f2420a36d
AmiteshSingh45 <amiteshsinghrajput00831@gmail.com>
2025-08-18 15:22:25 +0530

    checkhing register page on deplyoment

--- app/Login/page.js
+++ app/Login/page.js
@@ -15,7 +15,7 @@ export default function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("http://localhost:4000/api/v1/auth/login", {
+      const res = await axios.post("https://cupcakery-backend.onrender.com/api/v1/auth/login", {
         email,
         password,
       });
